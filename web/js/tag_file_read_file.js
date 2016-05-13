d8fe3dc185bc2b5f090f50b9736081f7379813fe
NK <beohemian@gmail.com>
2016-05-13 22:01:21 +0900

    Log 파일을 입력해서 확인할 수 있도록 수정

--- web/js/tag_file_read_file.js
+++ web/js/tag_file_read_file.js
@@ -14,9 +14,25 @@ function handleFileSelect(evt) {
         //var output = createDurationTime(files[i]);
     }
 }
-
 var places = [];
 var placeDic = {};
+
+function createPlaceDataByLogs(logs) {
+    for (var n = 0; n < logs.length; n++) {
+        var elements = logs[n].split('\t');
+        var date = elements[0];
+        var lat = roundXL(parseFloat(elements[1]), 4);
+        var lng = roundXL(parseFloat(elements[2]), 4);
+        var label = elements[3];
+
+        var place = {id: n, date: date, lat: lat, lng: lng, label: label.replace("\n", "")};
+        places.push(place);
+        placeDic[[lat, lng].join()] = place;
+
+        addLogRow(place);
+    }
+    getNearbySearchAtPlace();
+}
 
 function createPlaceData(file) {
     var reader = new FileReader();
@@ -24,20 +40,9 @@ function createPlaceData(file) {
     reader.onload = function(progressEvent){
         var lines = this.result.split('\n');
 
-        for(var n = 0; n < lines.length; n++){
-            var elements = lines[n].split('\t');
-            var date = elements[0];
-            var lat = roundXL(parseFloat(elements[1]),4);
-            var lng = roundXL(parseFloat(elements[2]),4);
-            var label = elements[3];
+        createPlaceDataByLogs(lines);
 
-            var place = {id:n, date:date, lat:lat, lng:lng, label:label.replace("\n","")};
-            places.push(place);
-            placeDic[[lat, lng].join()] = place;
-
-            addLogRow(place);
-        }
-        getNearbySearchAtPlace()
+        getNearbySearchAtPlace();
         // loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyD4efBm5pPQFojapLkNtg71AOY2246SJrU&signed_in=true&callback=getNearbySearchAtPlace');
     };
 
@@ -48,9 +53,9 @@ function addLogRow(place) {
     $('#logTable > tbody:last').append(
         '<tr>' +
         '<td class="col-xs-4">' + place.date + '</td>' +
-        '<td class="col-xs-5">' +
+        '<td class="col-xs-4">' +
         '<a id="myLink" href="#" onclick="selectedLink(this.text, markersDic ,false);return false;">' + [place.lat, place.lng].join() + '</a></td>' +
-        '<td class="col-xs-4">' + place.label + '</a></td>' +
+        '<td class="col-xs-5">' + place.label + '</a></td>' +
             // '<td class="col-xs-3"> ' +
             // '<button type="button" class="btn btn-default" value = ' + placeType + ' onClick="openConceptNet(this)">ConceptNet</button>' +
         '</tr>');
@@ -67,13 +72,22 @@ function loadScript(src){
 }
 
 $(function() {
-
     $('#sel1').on('change', function(){
         var selected = $(this).find("option:selected").val();
         console.log(selected);
     });
 
 });
+
+$(document).ready(function(){
+    $('#btn_run').on('click', function(event) {
+        event.preventDefault(); // To prevent following the link (optional)
+        var inputLog = $('#input_log').val();
+        var logs = inputLog.split("\n");
+        createPlaceDataByLogs(logs);
+    });
+});
+
 
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
 // $('#file #file')e.addEventListener('change', handleFileSelect, false);
